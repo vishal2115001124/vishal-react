feddcb7e78523bfda17fc05faa625e391905e08d
vishal@03 <singhalshal42@gmail.com>
2024-04-09 00:50:10 +0530

    commit

--- 01_viteReact/src/App.jsx
+++ 01_viteReact/src/App.jsx
@@ -1,9 +1,13 @@
-import Chai from "./chai"
+import Chai from "./Chai"
 
 function App() {
+
+  const username = "chai aur code"
+  //below that is EVALUATED JS EXPRESSION
+
   return (
     <>
-    <h1> Vishal REact</h1>
+    <h1> Vishal REact {username}</h1>
     <Chai/>
     </>
   )
--- 01_viteReact/src/main.jsx
+++ 01_viteReact/src/main.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return (
+    <>
+      <div>
+        <h1>Custom App</h1>
+      </div>
+    </>
+  )
+}
+
+const ReactElement = {
+  type : 'a',
+  props : {
+    href : "https://google.com",
+    target: '_blank'
+  },
+  Children : 'click me to visit google'
+}
+
+const anotherEleemt = (
+  <a href="https://google.com" target='_blank'>visit google</a>
+)
+
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(
+  'a',
+  {href:'https://google.com' , traget:'_blank'},
+  'click me to visit google',
+  anotherUser
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    // <App />
+  // </React.StrictMode>,
+
+  // MyApp(),
+  // <MyApp/>
+
+  // anotherEleemt
+
+  reactElement
 )
